92bc021604e79fb908e0f2d12161f5fe4345be97
mateo1198 <mateo1198@gmail.com>
2021-01-30 00:28:02 +0100

    ConstantSpeed script generico

--- Assets/Scripts/ConstantSpeed.cs
+++ Assets/Scripts/ConstantSpeed.cs
@@ -4,21 +4,15 @@ using UnityEngine;
 
 public class ConstantSpeed : MonoBehaviour
 {
-    [SerializeField]
-    private float speed = 1f;
+    // velocidad a la que nos moveremos
+    // valor modificable desde otros scripts
+    public float speed = 0f;
 
+    // vector de direccion (eje Z en este caso)
     private Vector3 velocity = new Vector3(0, 0, 1);
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
         transform.Translate(velocity * speed * Time.deltaTime);
     }
-
 }
