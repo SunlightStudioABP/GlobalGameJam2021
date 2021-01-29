95bed73cb64a98f41a5dbc5e07d57b6679b9b2c9
David Navas <dnavasdiaz97@gmail.com>
2021-01-29 20:51:37 +0100

    Meeps mal

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private float distancia;
+
+    private float speed;
+
+    private void Start()
+    {
+        speed = target.gameObject.GetComponent<PlayerMovement>().GetSpeed();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        if((target.position - transform.position).magnitude > distancia)
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Meep.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meep : MonoBehaviour
+{
+    BoxCollider meep_collider;
+
+    private void Start()
+    {
+        meep_collider = gameObject.GetComponent<BoxCollider>();
+    }
+
+
+     void OnCollisionEnter(Collision collision)
+    {
+        print("HOLA");
+
+        if (collision.gameObject.CompareTag("Player"))
+            print("HOLA");
+
+    }
+
+
+}
