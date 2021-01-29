3977ee7a40d307f5fe17ca9c36c464e107a81145
mateo1198 <mateo1198@gmail.com>
2021-01-29 23:15:09 +0100

    Testear on trigger enter please

---
+++ Assets/Scripts/ConstantSpeed.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantSpeed : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1f;
+
+    private Vector3 velocity = new Vector3(0, 0, 1);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(velocity * speed * Time.deltaTime);
+    }
+
+}
---
+++ Assets/Scripts/MeepSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeepSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Create", 0f, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Create()
+    {
+        Instantiate(prefab);
+    }
+}
