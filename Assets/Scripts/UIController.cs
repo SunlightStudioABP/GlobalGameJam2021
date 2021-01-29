d6d07bdc14c6c42cfa15ce0dad02e5ce6300080e
David Navas <dnavasdiaz97@gmail.com>
2021-01-30 00:24:20 +0100

    Movida gorda chavos

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,7 +9,7 @@ public class UIController : MonoBehaviour
     public static UIController _instance = new UIController();
 
     [SerializeField]
-    private Text meepsText;
+    private TextMeshProUGUI meepsText;
 
     int meeps;
 
