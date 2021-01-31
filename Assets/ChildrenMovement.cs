a2df1295cdbfc6fcb37a28c9eb736e072e0ffd45
mateo1198 <mateo1198@gmail.com>
2021-01-31 01:15:23 +0100

    Generacion del suelo infinita

---
+++ Assets/ChildrenMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildrenMovement : MonoBehaviour
+{
+    public float speed = -2f;
+
+    // vector de direccion (eje Z en este caso)
+    public Vector3 velocity = new Vector3(0, 0, 1);
+
+    void Update()
+    {
+
+        foreach (Transform t in GetComponentsInChildren<Transform>())
+        {
+            if (t != this.transform)
+                t.Translate(velocity * speed * Time.deltaTime);
+        }
+    }
+}
